3626212e52636ac33ad20ba13323bef4db307bfc
kanu-e <chukwuemekakanu@gmail.com>
2021-03-08 23:52:51 -0600

    installed & imported redux and thnk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/portfolios')
+    .then(response => response.json())
+    .then (data => console.log(data))
+
+  }
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
